0ed36cdad5cc11ffe95bc26da54e57da5cabe186
Gavin Crooks <gecrooks@users.noreply.github.com>
2020-12-08 02:22:35 -0800

    Update about.py

--- gecrooks_python_template/about.py
+++ gecrooks_python_template/about.py
@@ -4,7 +4,7 @@
 # the LICENSE.txt file in the root directory of this source tree.
 
 # Command line interface for the about() function
-# > python -m python_template.about
+# > python -m gecrooks_python_template.about
 #
 # NB: This module should not be imported by any other code in the package
 # (else we will get multiple import warnings)
