8ef158d84f634bc28f03819ad2e54b8558f6f804
gecrooks <gec@threeplusone.com>
2021-01-06 09:33:46 -0800

    Add gecrooks-python-template version to initial commit message

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,14 +1,22 @@
 import subprocess
 
 
+def git_version():
+     out = subprocess.check_output(["git", "describe", "--tags"])
+     ver = out.decode('ascii').strip()
+     return ver
+
 def git_init_and_tag():
     """
     Invoke the initial git and tag with 0.0.0 to make an initial version.
     """
+    # Get version of parent rep
+    ver = git_version()
+
     subprocess.check_output("git init", shell=True, stderr=subprocess.STDOUT)
     subprocess.check_output("git add -A .", shell=True, stderr=subprocess.STDOUT)
     subprocess.check_output(
-        "git commit -m 'Initialize from gecrooks-python-template'",
+        f"git commit -m 'Initialize from gecrooks-python-template ({ver})'",
         shell=True,
         stderr=subprocess.STDOUT,
     )
