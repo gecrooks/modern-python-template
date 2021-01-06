f277c5ea3fd97395f95b5fc9821745043636f6a2
gecrooks <gec@threeplusone.com>
2021-01-06 09:45:14 -0800

    Minor tweaks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,22 +1,15 @@
 import subprocess
 
 
-def git_version():
-     out = subprocess.check_output(["git", "describe", "--tags"])
-     ver = out.decode('ascii').strip()
-     return ver
-
 def git_init_and_tag():
     """
     Invoke the initial git and tag with 0.0.0 to make an initial version.
     """
-    # Get version of parent rep
-    ver = git_version()
 
     subprocess.check_output("git init", shell=True, stderr=subprocess.STDOUT)
     subprocess.check_output("git add -A .", shell=True, stderr=subprocess.STDOUT)
     subprocess.check_output(
-        f"git commit -m 'Initialize from gecrooks-python-template ({ver})'",
+        f"git commit -m 'Initialize from gecrooks-python-template'",
         shell=True,
         stderr=subprocess.STDOUT,
     )
