c52158e5e10fbc1e8981f3f725ea4cd9f934c298
Gavin Crooks <gecrooks@users.noreply.github.com>
2021-01-06 09:47:09 -0800

    Add support for github style 'main' master branch

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,10 +5,11 @@ def git_init_and_tag():
     """
     Invoke the initial git and tag with 0.0.0 to make an initial version.
     """
+
     subprocess.check_output("git init", shell=True, stderr=subprocess.STDOUT)
     subprocess.check_output("git add -A .", shell=True, stderr=subprocess.STDOUT)
     subprocess.check_output(
-        "git commit -m 'Initialize from gecrooks-python-template'",
+        f"git commit -m 'Initialize from gecrooks-python-template'",
         shell=True,
         stderr=subprocess.STDOUT,
     )
