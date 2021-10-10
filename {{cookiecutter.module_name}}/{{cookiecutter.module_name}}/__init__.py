09f32e2b3cdfb5001d2fe23f152a39aee510ba97
gecrooks <gec@threeplusone.com>
2021-10-10 16:49:41 -0700

    Add python 3.7 compatability

--- {{cookiecutter.module_name}}/{{cookiecutter.module_name}}/__init__.py
+++ {{cookiecutter.module_name}}/{{cookiecutter.module_name}}/__init__.py
@@ -3,5 +3,4 @@
 # This source code is licensed under the {{cookiecutter.license}} License
 # found in the LICENSE file in the root directory of this source tree.
 
-from .about_ import about  # noqa: F401
-from .config import __version__  # noqa: F401
+from .config import *  # noqa: F401, F403
