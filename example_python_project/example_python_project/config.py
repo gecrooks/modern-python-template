a556cee1745f61e0e3764d9d8988fe6860fc8b01
gecrooks <gec@threeplusone.com>
2022-07-25 19:32:13 -0700

    m

--- example_python_project/example_python_project/config.py
+++ example_python_project/example_python_project/config.py
@@ -1,4 +1,4 @@
-# Copyright 2021, Some Body and contributors
+# Copyright 2022, Some Body and contributors
 #
 # This source code is licensed under the Apache-2.0 License
 # found in the LICENSE file in the root directory of this source tree.
